1a608771498ebc769544332de61b8ff98fd53526
john.cornell <john.cornell@smokeball.com>
2023-06-07 20:44:58 +1000

    Add project files.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GPTEngine;
+
+namespace Boots.ViewModels
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private readonly GPT _gpt;
+
+        private readonly ObservableCollection<string> _history;
+
+        public ICommand SendToGPT { get; set; }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
